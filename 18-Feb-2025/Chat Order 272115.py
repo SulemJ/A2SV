4b32d0db04e55f06b38ce2972b1eef645ebf1064
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-02-18 17:57:22 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 18-Feb-2025/Chat Order 272115.py
@@ -0,0 +1,13 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+from collections import OrderedDict
+t = int(input())
+d =OrderedDict()
+for _ in range(t):
+    n = input()
+    if n in d:
+        del d[n]
+    d[n]=None
+
+for i in reversed(d):
+    print(i)
