7abf00e023071d934ee260699a16a0e665ba10d6
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-02-25 09:12:52 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 25-Feb-2025/Fox And Snake 282206.py
@@ -0,0 +1,23 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+
+n,m=map(int,input().split(" "))
+a=[]
+t=True
+for i in range(n):
+    if i %2==0:
+        a.append("#"*(m))
+
+    else:
+        if t==True:
+            s="."*(m-1)+"#"
+            t=False
+        else:
+            s="#"+"."*(m-1)
+            t=True
+        a.append(s)
+
+
+for i in a:
+    print(i)
+
