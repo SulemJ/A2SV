0fbb172488195b95b1fb6506570bf794ec4a676d
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-03-18 15:44:43 +0300

    Created 'Boats to Save People' by A2SV Hub

---
+++ 18-Mar-2025/Boats to Save People 310889.py
@@ -0,0 +1,19 @@
+# Problem: Boats to Save People - https://leetcode.com/problems/boats-to-save-people/
+
+class Solution:
+    def numRescueBoats(self, p: List[int], l: int) -> int:
+        p.sort()
+        i,j=0,len(p)-1
+        c=0
+        while i<=j:
+            if p[j]==l:
+                c+=1
+                j-=1
+            elif p[j]+p[i]<=l:
+                c+=1
+                i+=1
+                j-=1
+            elif p[j]+p[i]>l:
+                c+=1
+                j-=1
+        return c
