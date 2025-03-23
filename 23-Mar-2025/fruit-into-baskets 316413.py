b69aa9b098458fc48973f0c77c17d34f16915f61
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-03-23 23:59:01 +0300

    Created 'fruit-into-baskets' by A2SV Hub

---
+++ 23-Mar-2025/fruit-into-baskets 316413.py
@@ -0,0 +1,18 @@
+# Problem: fruit-into-baskets - https://leetcode.com/problems/fruit-into-baskets/
+
+class Solution:
+    def totalFruit(self, f: List[int]) -> int:
+        l,r,c=0,0,0
+        d={}
+        for i in range(len(f)):
+            if f[i] not in d:
+                d[f[i]]=0
+            d[f[i]]+=1
+            while len(d) ==3:
+                if d[f[l]] ==1:
+                    del d[f[l]]
+                else:
+                    d[f[l]]-=1
+                l+=1
+            c=max(c,i-l+1)
+        return c
