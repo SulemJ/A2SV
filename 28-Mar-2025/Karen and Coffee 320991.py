c10cf33de7bd46decd4d29cb3586d8ad71d6aef7
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-03-28 18:35:57 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 28-Mar-2025/Karen and Coffee 320991.py
@@ -0,0 +1,34 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+from collections import Counter
+import sys 
+input = sys.stdin.readline
+l,r,w=map(int, input().split())
+q=[0]*200002
+for _ in range(l):
+    a,b=map(int, input().split())
+    q[a]+=1
+    if b+1 <=200000:
+        q[b+1]-=1
+# j=[i for i in g for g in q]
+# print(q)
+s=[]
+p=0
+for i in q:
+    s.append(i+p)
+    p+=i
+# print(s)
+admissible = [0] * (200000 +1)
+prefix = [0] * (200000 +1)
+for t in range(1, 200000 +1):
+    admissible[t] = 1 if s[t] >= r else 0
+    prefix[t] = prefix[t-1] + admissible[t]
+    
+    # Process queries
+output = []
+for _ in range(w):
+    a, b = map(int,  input().split())
+    # ptr +=2
+    res = prefix[b] - prefix[a-1]
+    output.append(str(res))
+print('\n'.join(output))
