e63b18922f1ca6518217a10d499f304f00d2395e
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-04-05 01:02:59 +0300

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 04-Apr-2025/Sereja and Dima 326601.py
@@ -0,0 +1,24 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+t=int(input())
+n=list(map(int, input().split()))
+l,r,s,d=0,len(n)-1,0,0
+turn=True
+while l<=r:
+    if n[l]>=n[r]:
+        if turn:
+            s+=n[l]
+            turn=False
+        else:
+            d+=n[l]
+            turn=True
+        l+=1
+    else:
+        if turn:
+            s+=n[r]
+            turn=False
+        else:
+            d+=n[r]
+            turn=True
+        r-=1
+print(s,d)
