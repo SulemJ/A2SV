dfae12870232f2c7a446ad74e01a1c2c87748a77
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-04-06 00:18:59 +0300

    Created 'Kefa and Company' by A2SV Hub

---
+++ 05-Apr-2025/Kefa and Company 327110.py
@@ -0,0 +1,25 @@
+# Problem: Kefa and Company - https://codeforces.com/problemset/problem/580/B
+
+n, d = map(int, input().split())
+friends = []
+for _ in range(n):
+    m, s = map(int, input().split())
+    friends.append((m, s))
+
+friends.sort()
+
+prefix = [0] * (n + 1)
+for i in range(1, n+1):
+    prefix[i] = prefix[i-1] + friends[i-1][1]
+
+max_friendship = 0
+left = 0
+
+for right in range(n):
+    while friends[right][0] - friends[left][0] >= d:
+        left += 1
+    current_sum = prefix[right+1] - prefix[left]
+    if current_sum > max_friendship:
+        max_friendship = current_sum
+
+print(max_friendship)
