06df01d18e27cb9e934104b30a243003f10cc9e7
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-07-04 21:07:50 +0300

    Created 'Keys and Rooms' by A2SV Hub

---
+++ 04-Jul-2025/Keys and Rooms 373392.py
@@ -0,0 +1,15 @@
+# Problem: Keys and Rooms - https://leetcode.com/problems/keys-and-rooms/
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        queue = deque([0])
+        visited = set([0])
+        
+        while queue:
+            room = queue.popleft()
+            for key in rooms[room]:
+                if key not in visited:
+                    visited.add(key)
+                    queue.append(key)
+        
+        return len(visited) == len(rooms)
