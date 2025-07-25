c607769841d03aec9d8295f49e8ad55464f04297
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-07-25 22:53:39 +0300

    Created 'Couse Schedule' by A2SV Hub

---
+++ 25-Jul-2025/Couse Schedule 379425.py
@@ -0,0 +1,31 @@
+# Problem: Couse Schedule - https://leetcode.com/problems/course-schedule/
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        incoming = [0 for _ in range(numCourses)]
+        queue = deque()
+        order=[]
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            incoming[course]+=1
+
+        for i in range(numCourses):
+            if incoming[i] == 0:
+                queue.append(i)
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+
+            for nei in graph[course]:
+                incoming[nei]-=1
+                if incoming[nei] == 0:
+                    queue.append(nei)
+        if len(order) != numCourses:
+            return False 
+        else:
+            return True
+        #     return []
+        # return order    
