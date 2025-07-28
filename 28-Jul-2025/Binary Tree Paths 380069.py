84cd083c91c24ccbd7db21418156e5a320cc96f7
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-07-28 21:54:31 +0300

    Created 'Binary Tree Paths' by A2SV Hub

---
+++ 28-Jul-2025/Binary Tree Paths 380069.py
@@ -0,0 +1,27 @@
+# Problem: Binary Tree Paths - https://leetcode.com/problems/binary-tree-paths/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        path=[]
+        ans=[]
+
+        def helper(root):
+            if not root:
+                return
+
+            path.append(str(root.val))
+            if not root.left and not root.right:
+                ans.append("->".join(path))
+            else:
+                helper(root.left)
+                helper(root.right)
+            path.pop()
+
+        helper(root)
+        return ans
