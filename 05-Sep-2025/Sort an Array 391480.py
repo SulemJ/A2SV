f90cbe0521c0dd53f45ea4fa74640eafc24f17f6
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-09-05 22:14:53 +0300

    Created 'Sort an Array' by A2SV Hub

---
+++ 05-Sep-2025/Sort an Array 391480.py
@@ -0,0 +1,17 @@
+# Problem: Sort an Array - https://leetcode.com/problems/sort-an-array/description/
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        middle = len(nums) // 2
+        left = self.sortArray(nums[:middle])
+        right = self.sortArray(nums[middle:])
+        merged = []
+        while left and right:
+            if left[0] <= right [0]:
+                merged.append(left.pop(0))
+            else:
+                merged.append(right.pop(0))
+        merged.extend(right if right else left)
+        return merged
