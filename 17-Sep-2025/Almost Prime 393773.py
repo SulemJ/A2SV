48638b61a6e06b1917729d5f4542a1df537413bb
Sulem Jibril <sulemjibrildawud@gmail.com>
2025-09-17 22:59:11 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 17-Sep-2025/Almost Prime 393773.py
@@ -0,0 +1,13 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+t= int(input())
+list=[0]*(t+1)
+for i in range(2, t):
+    if list[i]==0:
+        for j in range(i, t+1,i):
+            list[j]+=1
+s=0
+for x in list:
+    if x ==2:
+        s+=1
+print(s)
